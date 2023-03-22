822fd5587a4cea78e3c4afd2f58dde03e44f8bfb
leonardog9823 <guanuco43@gmail.com>
2023-03-22 01:47:51 -0300

    learn routerLink

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing-module';
 
 import { AppComponent } from './app.component';
 import { PaisModule } from './pais/pais.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,10 @@ import { PaisModule } from './pais/pais.module';
   ],
   imports: [
     BrowserModule,
-    PaisModule
+    AppRoutingModule,
+
+    PaisModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
